e74009fa7570906c965af987e89eb9e8ffb46187
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-02 13:53:34 +0100

    [add] #1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 console.log('collegato ok');
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            msg: 'Ciao Vue!',
+            formatText:'format-text',
+        }
+    },
+    methods: {
+    },
+}).mount('#app');
