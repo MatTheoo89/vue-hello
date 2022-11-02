2b2d9b259910aa1e17d48bb947e7dcc5571238c4
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-02 16:01:49 +0100

    [add]#bonus 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,11 @@ createApp({
         return {
             msg: 'Ciao Vue!',
             formatText:'format-text',
+            path: 'assets/img/',
+            nameImage:'you_and_me.jpg',
         }
     },
     methods: {
+        
     },
 }).mount('#app');
